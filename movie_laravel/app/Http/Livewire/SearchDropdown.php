219e7491ac8c580264114a1fac147ce61515828c
giorgi <giorgimegrelishvili34@gmail.com>
2021-01-11 22:53:21 +0400

    refined_search

--- movie_laravel/app/Http/Livewire/SearchDropdown.php
+++ movie_laravel/app/Http/Livewire/SearchDropdown.php
@@ -10,8 +10,7 @@ class SearchDropdown extends Component
     public $search='';
     public function render()
     {
-        $searchResults=[];
-        $searchResults= Movie::where('name','like', '%'.$this->search.'%')->get();
+        $searchResults= Movie::where('name','like', '%'.$this->search.'%')->orWhere('year','like', '%'.$this->search.'%')->orWhere('duration','like', '%'.$this->search.'%')->orwhere('sound','like', '%'.$this->search.'%')->orWhere('director','like', '%'.$this->search.'%')->orWhere('in_the_cast','like', '%'.$this->search.'%')->orwhere('description','like', '%'.$this->search.'%')->get();
         return view('livewire.search-dropdown', [
             'searchResults'=>collect($searchResults)->take(7),
         ]);
